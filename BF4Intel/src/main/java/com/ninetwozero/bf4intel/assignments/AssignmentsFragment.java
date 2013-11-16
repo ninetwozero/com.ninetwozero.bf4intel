9e5e9b614385b8d1c375f73d68ce3ee56750b2be
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-11-16 13:32:20 +0100

    Update AssignmentsFragment for clarification purposes

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/assignments/AssignmentsFragment.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/assignments/AssignmentsFragment.java
@@ -19,13 +19,13 @@ import java.util.Map;
 
 public class AssignmentsFragment extends BaseFragment {
 
-    private static final List<String> ASSIGNMENT_TYPE = new ArrayList<String>(Arrays.asList("bronze", "silver", "gold", "sp"));
+    private static final List<String> ASSIGNMENT_TYPES = new ArrayList<String>(Arrays.asList("bronze", "silver", "gold", "sp"));
 
     private Assignments assignments;
     private GridView gridView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_assignments, container, false);
         gridView = (GridView) view.findViewById(R.id.assignments_grid);
         return view;
@@ -34,6 +34,7 @@ public class AssignmentsFragment extends BaseFragment {
     @Override
     public void onResume() {
         super.onResume();
+
         BusProvider.getInstance().register(this);
         setupGrid();
     }
@@ -45,25 +46,25 @@ public class AssignmentsFragment extends BaseFragment {
     }
 
     @Subscribe
-    public void assignments(Assignments assignments) {
+    public void onAssignmentsLoaded(final Assignments assignments) {
         this.assignments = assignments;
         setupGrid();
     }
 
     private void setupGrid() {
-        AssignmentsAdapter adapter = new AssignmentsAdapter(assignments(), getActivity().getApplicationContext());
+        AssignmentsAdapter adapter = new AssignmentsAdapter(getAssignments(), getActivity().getApplicationContext());
         gridView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
     }
 
-    private List<Assignment> assignments() {
-        return assignments != null ? orderAssignments() : new ArrayList<Assignment>();
+    private List<Assignment> getAssignments() {
+        return assignments != null ? getSortedAssignments() : new ArrayList<Assignment>();
     }
 
-    private List<Assignment> orderAssignments() {
+    private List<Assignment> getSortedAssignments() {
         List<Assignment> orderedAssignments = new ArrayList<Assignment>();
         Map<String, List<String>> missions = assignments.getAssignmentCategory();
-        for(String assignmentType : ASSIGNMENT_TYPE) {
+        for(String assignmentType : ASSIGNMENT_TYPES) {
             List<String> groupedAssignments = missions.get(assignmentType);
             Collections.sort(groupedAssignments);
             orderedAssignments.addAll(cherryPickAssignments(groupedAssignments));
@@ -71,7 +72,8 @@ public class AssignmentsFragment extends BaseFragment {
         return orderedAssignments;
     }
 
-    private List<Assignment> cherryPickAssignments(List<String> groupedAssignments) {
+    /* TODO: Not clear what this code is doing */
+    private List<Assignment> cherryPickAssignments(final List<String> groupedAssignments) {
         List<Assignment> orderedGroup = new ArrayList<Assignment>();
         for(String key: groupedAssignments) {
             if(assignments.getAssignments().containsKey(key)) {
