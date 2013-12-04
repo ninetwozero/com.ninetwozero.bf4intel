f52be2b370863282db970dedf395bbe27dd0ab47
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-12-04 23:38:50 +0100

    Add emblems to repo, with both string- and image map

---
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/EmblemImageMap.java
@@ -0,0 +1,87 @@
+package com.ninetwozero.bf4intel.resources.maps;
+
+import com.ninetwozero.bf4intel.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmblemImageMap {
+    private static final Map<String, Integer> map = new HashMap<String, Integer>() {
+        {
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_01", R.drawable.emblem_drone);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_02", R.drawable.emblem_gunship);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_03", R.drawable.emblem_stealthjet);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_04", R.drawable.emblem_binocular);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_05", R.drawable.emblem_cracket);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_06", R.drawable.emblem_crown_3);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_07", R.drawable.emblem_raptor);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_08", R.drawable.emblem_t__rex);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_09", R.drawable.emblem_triceratops);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_10", R.drawable.emblem_eod_bot);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_11", R.drawable.emblem_eye);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_12", R.drawable.emblem_flames);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_13", R.drawable.emblem_gear);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_14", R.drawable.emblem_grenade);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_15", R.drawable.emblem_armored_jeep);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_16", R.drawable.emblem_fast_jeep);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_17", R.drawable.emblem_ifv);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_18", R.drawable.emblem_mobile_aa2);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_19", R.drawable.emblem_ground_quad);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_20", R.drawable.emblem_headshot);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_21", R.drawable.emblem_transport_heli);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_22", R.drawable.emblem_jam);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_23", R.drawable.emblem_lightning);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_24", R.drawable.emblem_lion2);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_25", R.drawable.emblem_longrange);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_26", R.drawable.emblem_mag);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_27", R.drawable.emblem_medal_frame);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_28", R.drawable.emblem_medal_ring);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_29", R.drawable.emblem_medal_ring_2);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_30", R.drawable.emblem_medal_ring_3);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_31", R.drawable.emblem_medal_shape_3);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_32", R.drawable.emblem_missile);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_33", R.drawable.emblem_reduce_fall);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_34", R.drawable.emblem_shield_9);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_35", R.drawable.emblem_shield_5);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_36", R.drawable.emblem_shield_8);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_37", R.drawable.emblem_snakestick_wings);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_38", R.drawable.emblem_specialist);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_39", R.drawable.emblem_stationary_aa_2);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_40", R.drawable.emblem_stationary_mg_2);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_41", R.drawable.emblem_stationary_tow_2);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_42", R.drawable.emblem_suppression);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_43", R.drawable.emblem_sword);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_44", R.drawable.emblem_team_play);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_45", R.drawable.emblem_timer);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_46", R.drawable.emblem_aav);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_47", R.drawable.emblem_rhib_boat);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_48", R.drawable.emblem_dmr);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_49", R.drawable.emblem_wheel_3);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_50", R.drawable.emblem_wreath_3);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_51", R.drawable.emblem_attack_jet);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_52", R.drawable.emblem_explosion);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_53", R.drawable.emblem_fleur_de_lis);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_54", R.drawable.emblem_mlr);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_55", R.drawable.emblem_tank);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_56", R.drawable.emblem_handshake);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_57", R.drawable.emblem_attack_heli);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_58", R.drawable.emblem_hooah);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_59", R.drawable.emblem_lion);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_60", R.drawable.emblem_mine);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_61", R.drawable.emblem_square_corner);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_62", R.drawable.emblem_half_circle);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_63", R.drawable.emblem_line);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_64", R.drawable.emblem_crescent_moon);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_65", R.drawable.emblem_drop);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_66", R.drawable.emblem_bent_line);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_67", R.drawable.emblem_right_triangle);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_68", R.drawable.emblem_cracket);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_69", R.drawable.emblem_angry_fist);
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_70", R.drawable.emblem_epic_goat);
+        }
+    };
+
+    public static int get(final String key) {
+        return map.containsKey(key)? map.get(key) : R.drawable.acc_none;
+    }
+}
---
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/EmblemStringMap.java
@@ -0,0 +1,85 @@
+package com.ninetwozero.bf4intel.resources.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmblemStringMap {
+    private static final Map<String, String> map = new HashMap<String, String>() {
+        {
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_01", "Drone");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_02", "Gunship");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_03", "Stealthjet");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_04", "Binocular");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_05", "Cracket");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_06", "Crown 3");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_07", "Raptor");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_08", "T -rex");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_09", "Triceratops");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_10", "Eod Bot");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_11", "Eye");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_12", "Flames");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_13", "Gear");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_14", "Grenade");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_15", "Armored Jeep");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_16", "Fast Jeep");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_17", "Ifv");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_18", "Mobile Aa2");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_19", "Ground Quad");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_20", "Headshot");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_21", "Transport Heli");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_22", "Jam");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_23", "Lightning");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_24", "Lion2");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_25", "Longrange");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_26", "Mag");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_27", "Medal Frame");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_28", "Medal Ring");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_29", "Medal Ring 2");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_30", "Medal Ring 3");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_31", "Medal Shape 3");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_32", "Missile");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_33", "Reduce Fall");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_34", "Shield 9");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_35", "Shield 5");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_36", "Shield 8");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_37", "Snakestick Wings");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_38", "Specialist");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_39", "Stationary Aa 2");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_40", "Stationary Mg 2");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_41", "Stationary Tow 2");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_42", "Suppression");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_43", "Sword");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_44", "Team Play");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_45", "Timer");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_46", "Aav");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_47", "Rhib Boat");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_48", "Dmr");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_49", "Wheel 3");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_50", "Wreath 3");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_51", "Attack Jet");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_52", "Explosion");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_53", "Fleur-de-lis");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_54", "Mlr");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_55", "Tank");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_56", "Handshake");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_57", "Attack Heli");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_58", "Hooah");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_59", "Lion");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_60", "Mine");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_61", "Square Corner");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_62", "Half Circle");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_63", "Line");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_64", "Crescent Moon");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_65", "Drop");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_66", "Bent Line");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_67", "Right Triangle");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_68", "Sale!");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_69", "Angry Fist");
+            put("WARSAW_ID_M_BATTLEPACK_EMBLEM_70", "Epic Goat");
+        }
+    };
+
+    public static String get(final String key) {
+        return map.containsKey(key)? map.get(key) : key;
+    }
+}
