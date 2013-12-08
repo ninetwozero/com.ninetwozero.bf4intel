88e4cfc72b140c9f93985dad8b8fde559ad22434
Peter <peter.budo@gmail.com>
2013-12-08 13:53:10 +0000

    - Hide ProgressBar when weapon has at least one service star - Removed unlock progress I think it is not need it, and let progress bar to match parent

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/stats/weapons/WeaponStatsAdapter.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/stats/weapons/WeaponStatsAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ninetwozero.bf4intel.R;
@@ -24,17 +25,25 @@ public class WeaponStatsAdapter extends BaseIntelAdapter<Weapon> {
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.list_item_weapon, parent, false);
         }
-        Weapon ws = itemsList.get(position);
+        Weapon weapon = itemsList.get(position);
 
         ((TextView) view.findViewById(R.id.index)).setText(String.valueOf(position + 1));
 
-        ((ImageView)view.findViewById(R.id.weapon_image)).setImageResource(WeaponsImageMap.weaponsMap.get(ws.getUniqueName()));
+        ((ImageView)view.findViewById(R.id.weapon_image)).setImageResource(WeaponsImageMap.weaponsMap.get(weapon.getUniqueName()));
 
-        ((TextView) view.findViewById(R.id.service_star_count)).setText(String.valueOf(ws.getServiceStarsCount()));
+        ((TextView) view.findViewById(R.id.service_star_count)).setText(String.valueOf(weapon.getServiceStarsCount()));
 
-        ((TextView) view.findViewById(R.id.weapon_name)).setText(ws.getName().toUpperCase());
+        ((TextView) view.findViewById(R.id.weapon_name)).setText(weapon.getName().toUpperCase());
 
-        ((TextView)view.findViewById(R.id.weapon_kills)).setText(String.valueOf(ws.getKills()));
+        ((TextView)view.findViewById(R.id.weapon_kills)).setText(String.valueOf(weapon.getKills()));
+
+        if(weapon.getServiceStarsCount() > 0) {
+            view.findViewById(R.id.weapon_progress).setVisibility(View.GONE);
+        } else {
+            ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.weapon_progress);
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.setProgress(weapon.getServiceStarsProgress());
+        }
         return view;
     }
 }
