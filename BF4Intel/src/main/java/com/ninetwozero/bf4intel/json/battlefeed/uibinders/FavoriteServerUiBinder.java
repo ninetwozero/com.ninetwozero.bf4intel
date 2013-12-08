a5b6a0fedbf079929704e58ac2b392cf1eeb74b4
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-12-08 02:29:14 +0100

    Ooops, forgot to implement FavoriteServerEvent
    Oops, forgot to implement FavoriteServerEvent 100% - now it's displaying
    the *real* server names in the feed. :))

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/json/battlefeed/uibinders/FavoriteServerUiBinder.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/json/battlefeed/uibinders/FavoriteServerUiBinder.java
@@ -2,13 +2,15 @@ package com.ninetwozero.bf4intel.json.battlefeed.uibinders;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.TextView;
 
+import com.ninetwozero.bf4intel.R;
 import com.ninetwozero.bf4intel.interfaces.EventUiBinder;
-import com.ninetwozero.bf4intel.json.battlefeed.events.BattlePackEvent;
+import com.ninetwozero.bf4intel.json.battlefeed.events.FavoriteServerEvent;
 
-public class FavoriteServerUiBinder implements EventUiBinder<BattlePackEvent> {
+public class FavoriteServerUiBinder implements EventUiBinder<FavoriteServerEvent> {
     @Override
-    public void populateView(final Context context, final View view, final BattlePackEvent event) {
-        // TODO
+    public void populateView(final Context context, final View view, final FavoriteServerEvent event) {
+        ((TextView) view.findViewById(R.id.server_name)).setText(event.getServerName());
     }
 }
