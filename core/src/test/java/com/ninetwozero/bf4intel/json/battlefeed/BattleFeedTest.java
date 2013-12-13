974a339fa342cd41279c65ebda83ffa2e5018975
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-12-13 01:17:51 +0100

    [WIP] Add better tsting for Battle Feed

--- core/src/test/java/com/ninetwozero/bf4intel/json/battlefeed/BattleFeedTest.java
+++ core/src/test/java/com/ninetwozero/bf4intel/json/battlefeed/BattleFeedTest.java
@@ -5,30 +5,63 @@ import com.google.gson.GsonBuilder;
 import com.ninetwozero.bf4intel.util.IntelJsonParser;
 import com.ninetwozero.bf4intel.utils.FeedItemDeserializer;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 
 public class BattleFeedTest {
+    private static BattleFeed battleFeed;
+    private static final List<EventType> validTypes = new ArrayList<EventType>() {{
+        add(EventType.LEVEL_COMPLETE);
+        add(EventType.ADDED_FAV_SERVER);
+        add(EventType.SHARED_GAME_EVENT);
+        add(EventType.SHARED_GAME_EVENT);
+        add(EventType.GAME_REPORT);
+        add(EventType.BECAME_FRIENDS);
+        add(EventType.GAME_ACCESS);
+        add(EventType.RECEIVED_WALL_POST);
+        add(EventType.COMMENTED_GAME_REPORT);
+        add(EventType.RANKED_UP);
+        add(EventType.STATUS_MESSAGE);
+        add(EventType.BATTLE_PACK);
+        add(EventType.SHARED_GAME_EVENT);
+        add(EventType.WROTE_FORUM_POST);
+        add(EventType.CREATED_FORUM_THREAD);
+        add(EventType.COMMENTED_BLOG);
+    }};
 
-    @Test
-    public void should_parse_Json() throws IOException {
+    @BeforeClass
+    public static void setup() throws IOException {
         final GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(FeedItem.class, new FeedItemDeserializer());
         final Gson gson = gsonBuilder.create();
-        final BattleFeed battleFeed = IntelJsonParser.parse(
+        battleFeed = IntelJsonParser.parse(
             "/json/battlefeed.json",
             BattleFeed.class,
             gson
         );
+    }
 
-        assertEquals(battleFeed.getUserId(), "2832658801548551060");
-        assertEquals(battleFeed.getFeedItems().size(), 10);
-        assertEquals(
-            battleFeed.getFeedItems().get(0).getEvent().getEventType(),
-            EventType.LEVEL_COMPLETE
-        );
+    @Test
+    public void testThatWeHaveCorrectFeedItemTypesinList() throws IOException {
+        for (int i = 0, max = battleFeed.getFeedItems().size(); i < max; i++) {
+            // TODO: Remove when finished debugging
+            System.out.println("LOOP#" + i);
+            System.out.println("get(i) => " + battleFeed.getFeedItems().get(i));
+            System.out.println("getEvent() => " + battleFeed.getFeedItems().get(i).getEvent());
+            System.out.println("getEventType() => " + battleFeed.getFeedItems().get(i).getEvent().getEventType());
+            /*
+            TODO: LOOP#7: get(i) => null
+             */
+            assertEquals(
+                validTypes.get(i),
+                battleFeed.getFeedItems().get(i).getEvent().getEventType()
+            );
+        }
     }
 }
