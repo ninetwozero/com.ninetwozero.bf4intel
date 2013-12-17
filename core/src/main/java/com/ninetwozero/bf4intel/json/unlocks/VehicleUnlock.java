972ef27690965976542db45630ab1a2a7ac042c0
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-12-17 17:22:12 +0100

    [WIP] Start work on POJO

---
+++ core/src/main/java/com/ninetwozero/bf4intel/json/unlocks/VehicleUnlock.java
@@ -0,0 +1,4 @@
+package com.ninetwozero.bf4intel.json.unlocks;
+
+public class VehicleUnlock {
+}
--- core/src/main/java/com/ninetwozero/bf4intel/json/unlocks/WeaponUnlocks.java
+++ core/src/main/java/com/ninetwozero/bf4intel/json/unlocks/WeaponUnlocks.java
@@ -1,4 +1,19 @@
 package com.ninetwozero.bf4intel.json.unlocks;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WeaponUnlocks {
+    @SerializedName("personaId")
+    private long personaId;
+    @SerializedName("personaName")
+    private String personaName;
+    /*@SerializedName("allMissions")
+    private Map<String, Assignment> assignments = new HashMap<String, Assignment>();
+    */
+    @SerializedName("weaponsByCategory")
+    private Map<String, List<WeaponUnlock>> weaponUnlocks = new HashMap<String, List<WeaponUnlock>>();
 }
