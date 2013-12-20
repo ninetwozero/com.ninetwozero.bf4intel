ee485738bb643c290406bba263d211477458d336
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-12-20 01:57:28 +0100

    Correct faulty keys in UnlockCriteriaStringMap

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/UnlockCriteriaStringMap.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/UnlockCriteriaStringMap.java
@@ -12,7 +12,8 @@ public class UnlockCriteriaStringMap {
             put("sc_vehicleaboat", R.string.unlock_sc_vehicleaboat);
             put("sc_vehicleah", R.string.unlock_sc_vehicleah);
             put("sc_vehicleifv", R.string.unlock_sc_vehicleifv);
-            put("sc_vehiclejet", R.string.unlock_sc_vehiclejet);
+            put("sc_vehicleajet", R.string.unlock_sc_vehicleajet);
+            put("sc_vehiclesjet", R.string.unlock_sc_vehiclesjet);
             put("sc_vehiclembt", R.string.unlock_sc_vehiclembt);
             put("sc_vehiclesh", R.string.unlock_sc_vehiclesh);
         }
