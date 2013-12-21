95714b83ca8223f523269abe3f2dffcad0d3b197
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-12-21 02:33:30 +0100

    Update resource maps for unlocks

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/UnlockCriteriaStringMap.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/UnlockCriteriaStringMap.java
@@ -16,10 +16,19 @@ public class UnlockCriteriaStringMap {
             put("sc_vehiclesjet", R.string.unlock_sc_vehiclesjet);
             put("sc_vehiclembt", R.string.unlock_sc_vehiclembt);
             put("sc_vehiclesh", R.string.unlock_sc_vehiclesh);
+            put("sc_sniperrifles", R.string.unlock_sc_sniperrifles);
+            put("sc_lmgs", R.string.unlock_sc_lmgs);
+            put("sc_dmrs", R.string.unlock_sc_dmrs);
+            put("sc_pdws", R.string.unlock_sc_pdws);
+            put("sc_handgrenades", R.string.unlock_sc_handgrenades);
+            put("sc_handguns", R.string.unlock_sc_handguns);
+            put("sc_carbines", R.string.unlock_sc_carbines);
+            put("sc_shotguns", R.string.unlock_sc_shotguns);
+            put("sc_assaultrifles", R.string.unlock_sc_assaultrifles);
         }
     };
 
     public static int get(final String key) {
-        return map.containsKey(key) ? map.get(key) : R.string.na;
+        return map.containsKey(key) ? map.get(key) : R.string.empty;
     }
 }
