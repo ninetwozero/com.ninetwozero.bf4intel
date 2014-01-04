9a67656056401fe5414ca5077563b553d1b4ae02
Peter <peter.budo@gmail.com>
2014-01-04 14:55:47 +0000

    Battle reports initial layout change

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/stats/reports/BattleReportAdapter.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/stats/reports/BattleReportAdapter.java
@@ -1,17 +1,14 @@
 package com.ninetwozero.bf4intel.ui.stats.reports;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ninetwozero.bf4intel.R;
 import com.ninetwozero.bf4intel.json.battlereports.SummaryBattleReport;
-import com.ninetwozero.bf4intel.json.battlereports.Team;
 import com.ninetwozero.bf4intel.resources.maps.GameModeStringMap;
 import com.ninetwozero.bf4intel.resources.maps.LevelStringMap;
 import com.ninetwozero.bf4intel.utils.DateTimeUtils;
@@ -24,7 +21,6 @@ public class BattleReportAdapter extends BaseAdapter {
     private List<SummaryBattleReport> items;
 
     private String personaId;
-    private int[] teamWraps = new int[] {R.id.wrap_team1, R.id.wrap_team2};
 
     public BattleReportAdapter(final Context context, final String personaId) {
         this.context = context;
@@ -49,43 +45,19 @@ public class BattleReportAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(final int position, View convertView, final ViewGroup parent) {
+    public View getView(final int position, View view, final ViewGroup parent) {
         final SummaryBattleReport report = getItem(position);
-        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.list_item_battlereport, parent, false);
+        if (view == null) {
+            view = layoutInflater.inflate(R.layout.list_item_battlereport, parent, false);
         }
 
-        ((TextView) convertView.findViewById(R.id.map_name)).setText(LevelStringMap.get(report.getMap()));
-        ((TextView) convertView.findViewById(R.id.game_mode)).setText(GameModeStringMap.get(report.getGameMode()));
-        ((TextView) convertView.findViewById(R.id.server_name)).setText(report.getServerName());
+        ((TextView) view.findViewById(R.id.map_name)).setText(LevelStringMap.get(report.getMap()));
+        ((TextView) view.findViewById(R.id.game_mode)).setText(GameModeStringMap.get(report.getGameMode()));
+        ((TextView) view.findViewById(R.id.server_name)).setText(report.getServerName());
 
-        for (int i = 0; i < teamWraps.length; i++) {
-            final int adjustedIndex = i+1;
-            final View wrap = convertView.findViewById(teamWraps[i]);
-            final Team team = report.getTeams().get(String.valueOf(adjustedIndex));
-            final TextView teamNameView = (TextView) wrap.findViewById(R.id.team);
-            final TextView teamScoreView = (TextView) wrap.findViewById(R.id.score);
-            final ProgressBar progressBar = (ProgressBar) wrap.findViewById(R.id.progress);
-
-            final boolean playerIsInTeam = report.isPlayerInTeam(personaId, team.getId());
-            final int statusColorId = team.isWinner()? R.color.green : R.color.red;
-            final int colorId = playerIsInTeam? statusColorId : R.color.black;
-            final int colorCode = context.getResources().getColor(colorId);
-            final int typeFaceId = playerIsInTeam? Typeface.BOLD : Typeface.NORMAL;
-
-            teamNameView.setText(team.getName());
-            teamNameView.setTextColor(colorCode);
-            teamNameView.setTypeface(null, typeFaceId);
-            teamScoreView.setText(String.valueOf(team.getFinalscore()));
-            teamScoreView.setTextColor(colorCode);
-            teamScoreView.setTypeface(null, typeFaceId);
-
-            progressBar.setMax(team.getStartingScore());
-            progressBar.setProgress(team.getFinalscore());
-        }
-        ((TextView) convertView.findViewById(R.id.round_time)).setText(DateTimeUtils.toLiteral(report.getDuration()));
-        ((TextView) convertView.findViewById(R.id.date)).setText(DateTimeUtils.toRelative(context, report.getDate()));
-        return convertView;
+        ((TextView) view.findViewById(R.id.round_time)).setText(DateTimeUtils.toLiteral(report.getDuration()));
+        ((TextView) view.findViewById(R.id.date)).setText(DateTimeUtils.toRelative(context, report.getDate()));
+        return view;
     }
 
     public void setItems(final List<SummaryBattleReport> items) {
