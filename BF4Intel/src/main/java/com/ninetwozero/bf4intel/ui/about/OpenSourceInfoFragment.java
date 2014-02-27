74a60a5ea8c05de66d05a2063b4b71d163e8c026
Karl Lindmark <karl.lindmark@ninetwozero.com>
2014-02-27 08:36:50 +0100

    [WIP] Trying out the open source list as html

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/about/OpenSourceInfoFragment.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/about/OpenSourceInfoFragment.java
@@ -1,15 +1,15 @@
 package com.ninetwozero.bf4intel.ui.about;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.ninetwozero.bf4intel.R;
-import com.ninetwozero.bf4intel.ui.about.datatypes.HeaderAboutRow;
-import com.ninetwozero.bf4intel.ui.about.datatypes.SimpleAboutRow;
+import com.ninetwozero.bf4intel.base.ui.BaseFragment;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class OpenSourceInfoFragment extends BaseAboutFragment {
+public class OpenSourceInfoFragment extends BaseFragment {
 
     public static OpenSourceInfoFragment newInstance() {
         final OpenSourceInfoFragment fragment = new OpenSourceInfoFragment();
@@ -21,88 +21,18 @@ public class OpenSourceInfoFragment extends BaseAboutFragment {
 
     }
 
+
     @Override
-    protected List<HeaderAboutRow> getHeadersForList() {
-        final List<HeaderAboutRow> items = new ArrayList<HeaderAboutRow>();
-        items.add(new HeaderAboutRow(R.string.opensource_label_self, 1));
-        items.add(new HeaderAboutRow(R.string.opensource_label_libraries, 9));
-        return items;
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle state) {
+        super.onCreateView(inflater, parent, state);
+
+        final View view = inflater.inflate(R.layout.fragment_open_source, parent, false);
+        initialize(view);
+        return view;
     }
 
-    @Override
-    protected List<SimpleAboutRow> getItemsForList() {
-        final List<SimpleAboutRow> items = new ArrayList<SimpleAboutRow>();
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_self,
-                R.string.opensource_subtitle_self,
-                "https://github.com/karllindmark/com.ninetwozero.bf4intel"
-            )
-        );
-
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_bugsense,
-                R.string.opensource_subtitle_bugsense,
-                "http://www.bugsense.com"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_cupboard,
-                R.string.opensource_subtitle_cupboard,
-                "https://bitbucket.org/qbusict/cupboard/"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_gson,
-                R.string.opensource_subtitle_gson,
-                "https://code.google.com/p/google-gson"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_httprequest,
-                R.string.opensource_subtitle_httprequest,
-                "https://github.com/kevinsawicki/http-request"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_jsoup,
-                R.string.opensource_subtitle_jsoup,
-                "http://jsoup.org/"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_otto,
-                R.string.opensource_subtitle_otto,
-                "http://square.github.io/otto/"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_pagerslidingtabstrip,
-                R.string.opensource_subtitle_pagerslidingtabstrip,
-                "https://github.com/astuetz/PagerSlidingTabStrip"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_picasso,
-                R.string.opensource_subtitle_picasso,
-                "http://square.github.io/picasso/"
-            )
-        );
-        items.add(
-            new SimpleAboutRow(
-                R.string.opensource_title_stickygridheaders,
-                R.string.opensource_subtitle_stickygridheaders,
-                "https://github.com/TonicArtos/StickyGridHeaders"
-            )
-        );
-        return items;
+    private void initialize(View view) {
+        final WebView webView = (WebView) view.findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/licenses.htm");
     }
 }
