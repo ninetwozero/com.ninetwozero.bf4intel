5f77026719e8f1f86768cbd16fe51b5a0b3efd4f
Peter Miklosko <peter.budo@gmail.com>
2014-03-24 14:34:18 +0000

    Removed obsolete imports

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/ranks/RankImageMap.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/ranks/RankImageMap.java
@@ -2,11 +2,8 @@ package com.ninetwozero.bf4intel.resources.maps.ranks;
 
 import com.ninetwozero.bf4intel.R;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class RankImageMap {
     private static final Map<String, Integer> images = new HashMap<String, Integer>() {
