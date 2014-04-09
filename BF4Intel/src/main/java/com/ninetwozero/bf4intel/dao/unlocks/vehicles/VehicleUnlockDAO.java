e3b347ffa8deec9fcd6f8ce2d0a23b0bbaf6fbce
Karl Lindmark <karl.lindmark@ninetwozero.com>
2014-04-09 18:25:50 +0200

    [WIP] Migrate Sprinkles from 1.2.4 to 1.3.0-SNAPSHOT

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/dao/unlocks/vehicles/VehicleUnlockDAO.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/dao/unlocks/vehicles/VehicleUnlockDAO.java
@@ -5,21 +5,21 @@ import com.ninetwozero.bf4intel.json.unlocks.vehicles.SortedVehicleUnlocks;
 
 import se.emilsjolander.sprinkles.Model;
 import se.emilsjolander.sprinkles.annotations.Column;
-import se.emilsjolander.sprinkles.annotations.PrimaryKey;
+import se.emilsjolander.sprinkles.annotations.Key;
 import se.emilsjolander.sprinkles.annotations.Table;
 
 @Table("SoldierVehicleUnlocks")
 public class VehicleUnlockDAO extends Model {
     public static final String TABLE_NAME = "SoldierVehicleUnlocks";
 
-    @PrimaryKey
+    @Key
     @Column("soldierId")
     private String soldierId;
 
     @Column("soldierName")
     private String soldierName;
 
-    @PrimaryKey
+    @Key
     @Column("platformId")
     private int platformId;
 
---
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/database/migrations/Version095Migration.java
@@ -0,0 +1,45 @@
+package com.ninetwozero.bf4intel.database.migrations;
+
+import android.database.sqlite.SQLiteDatabase;
+
+
+import com.ninetwozero.bf4intel.dao.assignments.AssignmentsDAO;
+import com.ninetwozero.bf4intel.dao.awards.AwardsDAO;
+import com.ninetwozero.bf4intel.dao.stats.details.DetailedStatsDAO;
+import com.ninetwozero.bf4intel.dao.stats.vehicles.VehicleStatsDAO;
+import com.ninetwozero.bf4intel.dao.stats.weapons.WeaponStatsDAO;
+import com.ninetwozero.bf4intel.dao.unlocks.kits.KitUnlockDAO;
+import com.ninetwozero.bf4intel.dao.unlocks.vehicles.VehicleUnlockDAO;
+import com.ninetwozero.bf4intel.dao.unlocks.weapons.WeaponUnlockDAO;
+
+import se.emilsjolander.sprinkles.Migration;
+
+public class Version095Migration extends Migration {
+    @Override
+    protected void doMigration(SQLiteDatabase database) {
+        createRegularJsonTable(database, WeaponStatsDAO.TABLE_NAME);
+        createRegularJsonTable(database, VehicleStatsDAO.TABLE_NAME);
+        //createRegularJsonTable(database, BattleReportDAO.TABLE_NAME);
+        createRegularJsonTable(database, DetailedStatsDAO.TABLE_NAME);
+
+        createRegularJsonTable(database, WeaponUnlockDAO.TABLE_NAME);
+        createRegularJsonTable(database, VehicleUnlockDAO.TABLE_NAME);
+        createRegularJsonTable(database, KitUnlockDAO.TABLE_NAME);
+
+        createRegularJsonTable(database, AssignmentsDAO.TABLE_NAME);
+        createRegularJsonTable(database, AwardsDAO.TABLE_NAME);
+    }
+
+    private void createRegularJsonTable(SQLiteDatabase database, String tableName) {
+        database.execSQL(
+            "CREATE TABLE " + tableName + "(" +
+                "soldierId TEXT," +
+                "soldierName TEXT," +
+                "platformId INTEGER" +
+                "json TEXT," +
+                "version INTEGER," +
+                "PRIMARY KEY(soldierId, platformId)" +
+                ")"
+        );
+    }
+}
