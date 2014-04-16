72cfc4dacb4a5e5710bd4b7186f2c862fad175e6
Karl Lindmark <karl.lindmark@ninetwozero.com>
2014-04-16 09:38:15 +0200

    Add correct category sorting order

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/dao/unlocks/vehicles/VehicleUnlockMapSorter.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/dao/unlocks/vehicles/VehicleUnlockMapSorter.java
@@ -10,6 +10,13 @@ import java.util.List;
 import java.util.Map;
 
 public class VehicleUnlockMapSorter {
+    private static final String[] CATEGORY_ORDER = new String[] {
+        "Vehicle Air Helicopter Scout", "Vehicle Air Jet Attack",
+        "Vehicle Fast Attack Craft", "Vehicle Infantry Fighting Vehicle",
+        "Vehicle Air Jet Stealth", "Vehicle Air Helicopter Attack",
+        "Vehicle Main Battle Tanks", "Vehicle Anti Air"
+    };
+
     public static SortedVehicleUnlocks sort(final Map<String, List<VehicleUnlock>> unlockMap, final SortMode mode) {
         if (mode == SortMode.PROGRESS) {
             return sortItemsByProgress(unlockMap);
@@ -29,8 +36,10 @@ public class VehicleUnlockMapSorter {
 
     private static SortedVehicleUnlocks sortItemsByCategory(Map<String, List<VehicleUnlock>> unlockMap) {
         List<VehicleUnlock> list = new ArrayList<VehicleUnlock>();
-        for (String key : unlockMap.keySet()) {
-            list.addAll(unlockMap.get(key));
+        for (String key : CATEGORY_ORDER) {
+            if (unlockMap.containsKey(key)) {
+                list.addAll(unlockMap.get(key));
+            }
         }
         return new SortedVehicleUnlocks(list);
     }
