576e4bf31dbcef8ef016bf62544f91fef621c2c2
Karl Lindmark <karl.lindmark@ninetwozero.com>
2014-05-10 00:43:21 +0200

    Add WeaponInfoStringMap to the app
    Non-static as it would crowd the VM. Should this be converted to
    static? @peter-budo

---
+++ core/src/main/java/com/ninetwozero/bf4intel/datatypes/WeaponInfo.java
@@ -0,0 +1,62 @@
+package com.ninetwozero.bf4intel.datatypes;
+
+public class WeaponInfo {
+    public static final int NONE = -1;
+
+    private String category;
+    private int damage = NONE;
+    private int accuracy = NONE;
+    private int range = NONE;
+    private int hipFire = NONE;
+    private boolean singleFire;
+    private boolean burstFire;
+    private boolean autoFire;
+
+
+    public WeaponInfo(String category) {
+        this.category = category;
+    }
+
+    public WeaponInfo(String category, int damage, int accuracy, int range, int hipFire, boolean singleFire, boolean burstFire, boolean autoFire) {
+        this.category = category;
+        this.damage = damage;
+        this.accuracy = accuracy;
+        this.range = range;
+        this.hipFire = hipFire;
+        this.singleFire = singleFire;
+        this.burstFire = burstFire;
+        this.autoFire = autoFire;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getAccuracy() {
+        return accuracy;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getHipFire() {
+        return hipFire;
+    }
+
+    public boolean isSingleFire() {
+        return singleFire;
+    }
+
+    public boolean isBurstFire() {
+        return burstFire;
+    }
+
+    public boolean isAutoFire() {
+        return autoFire;
+    }
+}
