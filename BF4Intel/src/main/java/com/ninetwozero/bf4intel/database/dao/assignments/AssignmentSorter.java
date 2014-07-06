8da732f693d6335fdd44694578cd394364384036
Peter <peter.budo@gmail.com>
2014-07-06 16:42:42 +0100

    • Show assignments by progress as all uncompleted sorted by progress and followed by completed assignments sorted by groups

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/database/dao/assignments/AssignmentSorter.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/database/dao/assignments/AssignmentSorter.java
@@ -1,20 +1,19 @@
 package com.ninetwozero.bf4intel.database.dao.assignments;
 
+import android.util.Pair;
+
 import com.ninetwozero.bf4intel.database.dao.AbstractSorter;
 import com.ninetwozero.bf4intel.database.dao.unlocks.SortMode;
 import com.ninetwozero.bf4intel.json.assignments.Assignment;
 import com.ninetwozero.bf4intel.json.assignments.Assignments;
 import com.ninetwozero.bf4intel.json.assignments.SortedAssignmentContainer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AssignmentSorter extends AbstractSorter<SortedAssignmentContainer> {
     public static final List<String> ASSIGNMENT_TYPE = new ArrayList<String>(Arrays.asList("bronze", "silver", "gold", "sp"));
     private final Assignments assignments;
+    private Map<String, List<Assignment>> groupedMap = new HashMap<String, List<Assignment>>();
 
     public AssignmentSorter(final Assignments assignments) {
         this.assignments = assignments;
@@ -31,20 +30,36 @@ public class AssignmentSorter extends AbstractSorter<SortedAssignmentContainer> 
 
     @Override
     protected SortedAssignmentContainer sortByProgress() {
-        List<Assignment> uncompleted = fetchUncompletedAssignments();
+        if (groupedMap.size() == 0) {
+            sortByCategory();
+        }
+        List<Assignment> uncompleted = new ArrayList<Assignment>();
+        List<Assignment> completed = new ArrayList<Assignment>();
+        for (String group : ASSIGNMENT_TYPE) {
+            Pair<List<Assignment>, List<Assignment>> pair = fetchUncompletedAssignments(group);
+            uncompleted.addAll(pair.first);
+            completed.addAll(pair.second);
+        }
         Collections.sort(uncompleted);
+        uncompleted.addAll(completed);
         return new SortedAssignmentContainer(uncompleted, assignments.getExpansions());
     }
 
     @Override
     protected SortedAssignmentContainer sortByCategory() {
         List<Assignment> orderedAssignments = new ArrayList<Assignment>();
-        Map<String, List<String>> missions = assignments.getAssignmentCategory();
-        for (String group : ASSIGNMENT_TYPE) {
-            List<String> groupedAssignments = missions.get(group);
-            orderedAssignments.addAll(
-                fetchGroupedAssignments(groupedAssignments, group)
-            );
+        if (groupedMap.size() == 0) {
+            Map<String, List<String>> missions = assignments.getAssignmentCategory();
+            for (String group : ASSIGNMENT_TYPE) {
+                List<String> groupedAssignments = missions.get(group);
+                List<Assignment> assignmentList = fetchGroupedAssignments(groupedAssignments, group);
+                groupedMap.put(group, assignmentList);
+                orderedAssignments.addAll(assignmentList);
+            }
+        } else {
+            for (String group : ASSIGNMENT_TYPE) {
+                orderedAssignments.addAll(groupedMap.get(group));
+            }
         }
         return new SortedAssignmentContainer(orderedAssignments, assignments.getExpansions());
     }
@@ -61,14 +76,17 @@ public class AssignmentSorter extends AbstractSorter<SortedAssignmentContainer> 
         return orderedGroup;
     }
 
-    private List<Assignment> fetchUncompletedAssignments() {
-        List<Assignment> allAssignments = new ArrayList<Assignment>(assignments.getAssignments().values());
+    private Pair<List<Assignment>, List<Assignment>> fetchUncompletedAssignments(final String group) {
         List<Assignment> uncompleted = new ArrayList<Assignment>();
-        for (Assignment assignment : allAssignments) {
+        List<Assignment> completed = new ArrayList<Assignment>();
+        List<Assignment> assignmentsOfGroup = groupedMap.get(group);
+        for (Assignment assignment : assignmentsOfGroup) {
             if (assignment.getCompletion() != 100) {
                 uncompleted.add(assignment);
+            } else {
+                completed.add(assignment);
             }
         }
-        return uncompleted;
+        return new Pair<List<Assignment>, List<Assignment>>(uncompleted, completed);
     }
 }
