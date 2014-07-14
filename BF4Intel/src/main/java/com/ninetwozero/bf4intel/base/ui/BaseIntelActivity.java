e9b2c3a408342953878b8a1ec31fdff878d3a1be
Karl Lindmark <karl.lindmark@ninetwozero.com>
2014-07-14 16:23:51 +0200

    Remove unused import from BaseIntelActivity

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/base/ui/BaseIntelActivity.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/base/ui/BaseIntelActivity.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
