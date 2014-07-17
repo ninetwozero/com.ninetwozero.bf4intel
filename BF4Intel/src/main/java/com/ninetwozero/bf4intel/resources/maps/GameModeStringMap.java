c7cb387f82f2716468aa7d55789c469a102c2c1c
Peter <peter.budo@gmail.com>
2014-07-17 21:39:55 +0100

    • Added new game mode, this required to move from integer to long WTF!

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/GameModeStringMap.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/GameModeStringMap.java
@@ -4,23 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GameModeStringMap {
-    private static final Map<Integer, String> map = new HashMap<Integer, String>() {{
-        put(1, "Conquest");
-        put(2, "Rush");
-        put(8, "Squad Deathmatch");
-        put(32, "Team Deathmatch");
-        put(64, "Conquest Large");
-        put(1024, "Domination");
-        put(2048, "Hardcore Team Deathmatch");
-        put(524288, "Capture the Flag");
-        put(2097152, "Obliteration");
-        put(8388608, "Air Superiority");
-        put(33554432, "Carrier Assault");
-        put(67108864, "Carrier Assault Large");
-        put(16777216, "Defuse");
+    private static final Map<Long, String> map = new HashMap<Long, String>() {{
+        put(1l, "Conquest");
+        put(2l, "Rush");
+        put(8l, "Squad Deathmatch");
+        put(32l, "Team Deathmatch");
+        put(64l, "Conquest Large");
+        put(1024l, "Domination");
+        put(2048l, "Hardcore Team Deathmatch");
+        put(524288l, "Capture the Flag");
+        put(2097152l, "Obliteration");
+        put(8388608l, "Air Superiority");
+        put(33554432l, "Carrier Assault");
+        put(67108864l, "Carrier Assault Large");
+        put(16777216l, "Defuse");
+        put(34359738368l, "Chain Link");
     }};
 
-    public static String get(final int id) {
+    public static String get(final long id) {
         return map.containsKey(id)? map.get(id) : "N/A";
     }
 }
