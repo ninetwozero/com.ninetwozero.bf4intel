899a0dc83b316daeef22f9aa5670ffb0330d1493
Peter <peter.budo@gmail.com>
2014-07-23 23:09:17 +0100

    • Replaced l with L for readability

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/GameModeStringMap.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/GameModeStringMap.java
@@ -5,20 +5,20 @@ import java.util.Map;
 
 public class GameModeStringMap {
     private static final Map<Long, String> map = new HashMap<Long, String>() {{
-        put(1l, "Conquest");
-        put(2l, "Rush");
-        put(8l, "Squad Deathmatch");
-        put(32l, "Team Deathmatch");
-        put(64l, "Conquest Large");
-        put(1024l, "Domination");
-        put(2048l, "Hardcore Team Deathmatch");
-        put(524288l, "Capture the Flag");
-        put(2097152l, "Obliteration");
-        put(8388608l, "Air Superiority");
-        put(33554432l, "Carrier Assault");
-        put(67108864l, "Carrier Assault Large");
-        put(16777216l, "Defuse");
-        put(34359738368l, "Chain Link");
+        put(1L, "Conquest");
+        put(2L, "Rush");
+        put(8L, "Squad Deathmatch");
+        put(32L, "Team Deathmatch");
+        put(64L, "Conquest Large");
+        put(1024L, "Domination");
+        put(2048L, "Hardcore Team Deathmatch");
+        put(524288L, "Capture the Flag");
+        put(2097152L, "Obliteration");
+        put(8388608L, "Air Superiority");
+        put(33554432L, "Carrier Assault");
+        put(67108864L, "Carrier Assault Large");
+        put(16777216L, "Defuse");
+        put(34359738368L, "Chain Link");
     }};
 
     public static String get(final long id) {
