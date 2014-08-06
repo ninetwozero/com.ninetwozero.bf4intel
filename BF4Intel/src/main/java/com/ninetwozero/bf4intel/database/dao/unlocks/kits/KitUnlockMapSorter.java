060d7d0f04ead865ee7f9e7b5b50d449985baadb
Peter Miklosko <peter.budo@gmail.com>
2014-08-06 16:34:15 +0100

    Do de-duplication only once when data are fetched

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/database/dao/unlocks/kits/KitUnlockMapSorter.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/database/dao/unlocks/kits/KitUnlockMapSorter.java
@@ -13,13 +13,7 @@ import java.util.Set;
 
 public class KitUnlockMapSorter extends AbstractSorter<SortedKitUnlocks> {
     public static final String[] CATEGORY_ORDER = new String[] { "1", "2","32", "8"};
-    private static final Set<String> SKIP_LIST = new HashSet<String>() {
-        {
-            add("1BFBE29D-CA2D-42B8-88DD-8E5C481E40FC"); // AK5C
-            add("D9899D8B-2D67-4A13-975E-F0D5E8E1525D"); // RFB
-            add("9892E6C9-6317-4558-953D-94BA2DCD355C"); // QBS-09
-        }
-    };
+
 
     private Map<String, List<KitItemUnlockContainer>> unlockMap;
 
@@ -30,7 +24,7 @@ public class KitUnlockMapSorter extends AbstractSorter<SortedKitUnlocks> {
     protected SortedKitUnlocks sortByProgress() {
         List<KitItemUnlockContainer> list = new ArrayList<KitItemUnlockContainer>();
         for (String key : unlockMap.keySet()) {
-            final List<KitItemUnlockContainer> allUnlocks = removeDuplicates(unlockMap.get(key));
+            final List<KitItemUnlockContainer> allUnlocks = unlockMap.get(key);
             addKitIdToItems(allUnlocks, key);
             list.addAll(allUnlocks);
         }
@@ -43,7 +37,7 @@ public class KitUnlockMapSorter extends AbstractSorter<SortedKitUnlocks> {
         List<KitItemUnlockContainer> list = new ArrayList<KitItemUnlockContainer>();
         for (String key : CATEGORY_ORDER) {
             if (unlockMap.containsKey(key)) {
-                final List<KitItemUnlockContainer> allUnlocks = removeDuplicates(unlockMap.get(key));
+                final List<KitItemUnlockContainer> allUnlocks = unlockMap.get(key);
                 addKitIdToItems(allUnlocks, key);
                 list.addAll(allUnlocks);
             }
@@ -57,18 +51,5 @@ public class KitUnlockMapSorter extends AbstractSorter<SortedKitUnlocks> {
         }
     }
 
-    private static List<KitItemUnlockContainer> removeDuplicates(final List<KitItemUnlockContainer> containers) {
-        Set<String> guids = new HashSet<String>(SKIP_LIST);
-        List<KitItemUnlockContainer> uniqueList = new ArrayList<KitItemUnlockContainer>();
 
-        for (KitItemUnlockContainer container : containers) {
-            final String guid = container.getUnlock().getGuid();
-            if (guids.contains(guid)) {
-                continue;
-            }
-            guids.add(guid);
-            uniqueList.add(container);
-        }
-        return uniqueList;
-    }
 }
--- core/src/main/java/com/ninetwozero/bf4intel/json/unlocks/KitUnlocks.java
+++ core/src/main/java/com/ninetwozero/bf4intel/json/unlocks/KitUnlocks.java
@@ -9,6 +9,10 @@ public class KitUnlocks {
     @SerializedName("unlocks")
     private Map<String, List<KitItemUnlockContainer>> unlockMap;
 
+    public KitUnlocks(Map<String, List<KitItemUnlockContainer>> unlockMap){
+        this.unlockMap = unlockMap;
+    }
+
     public Map<String, List<KitItemUnlockContainer>> getUnlockMap() {
         return unlockMap;
     }
