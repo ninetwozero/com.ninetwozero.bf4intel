63473558639834ee658e420052f53f584df30b55
peter-budo <peter.budo@gmail.com>
2015-01-27 21:52:02 +0000

    [WIP] Fixed errors from previous commit, creating WeaponLineartImageMap (note for future, lets make sure we use Battlelog IDs not makeshift id/name convensions)

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/weapons/WeaponLineartImageMap.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/resources/maps/weapons/WeaponLineartImageMap.java
@@ -5,52 +5,32 @@ import com.ninetwozero.bf4intel.R;
 import java.util.HashMap;
 import java.util.Map;
 
-public class WeaponImageMap {
+public class WeaponLineartImageMap {
 
     private static final Map<String, Integer> map = new HashMap<String, Integer>() {
         {
-            put("WARSAW_ID_P_WNAME_ULTIM", R.drawable.w_ultimax_fancy);
-            put("WARSAW_ID_P_WNAME_CSLR4", R.drawable.w_cslr4_fancy);
-            put("WARSAW_ID_P_WNAME_ACR", R.drawable.w_acwr_fancy);
-            put("WARSAW_ID_P_WNAME_AK5C", R.drawable.w_ak5c_fancy);
-            put("WARSAW_ID_P_WNAME_QBS09", R.drawable.w_qbs09_fancy);
-            put("WARSAW_ID_P_WNAME_AK12", R.drawable.w_ak12_fancy);
-            put("WARSAW_ID_P_INAME_BAYONETT", R.drawable.w_knife_fancy);
-            put("WARSAW_ID_P_WNAME_RFBTARGET", R.drawable.w_rfb_fancy);
-            put("WARSAW_ID_P_WNAME_M412REX", R.drawable.w_mp412rex_fancy);
-            put("WARSAW_ID_P_INAME_C4", R.drawable.w_c4_fancy);
-            put("WARSAW_ID_P_INAME_IMPACT", R.drawable.w_rgo_impact_fancy);
-            put("WARSAW_ID_P_WNAME_QBZ951", R.drawable.w_qbz951_fancy);
-            put("WARSAW_ID_P_INAME_40MM", R.drawable.w_m320_fancy);
-            put("WARSAW_ID_P_INAME_XM25", R.drawable.w_xm25_fancy);
-            put("WARSAW_ID_P_INAME_MBTLAW", R.drawable.w_mbt_law_fancy);
-            put("WARSAW_ID_P_INAME_M32MGL", R.drawable.w_mgl_fancy);
-            put("WARSAW_ID_P_WNAME_M82A3MID", R.drawable.w_m82a3_med_fancy);
-            put("WARSAW_ID_P_INAME_FIM92", R.drawable.w_fim92_stinger_fancy);
-            put("WARSAW_ID_P_XP1_WNAME_L96A1", R.drawable.w_l96a1_fancy);
-            put("WARSAW_ID_P_WNAME_MX4", R.drawable.w_mx4_fancy);
-            put("WARSAW_ID_P_WNAME_P226", R.drawable.w_p226_fancy);
-            put("WARSAW_ID_P_WNAME_AMR2", R.drawable.w_amr_far_fancy);
-            put("WARSAW_ID_P_INAME_RPG7", R.drawable.w_rpg7_fancy);
+            put("WARSAW_ID_P_WNAME_ULTIM", R.drawable.w_lineart_u100_mk5);
+            put("WARSAW_ID_P_WNAME_CSLR4", R.drawable.w_lineart_cslr4);
+            put("WARSAW_ID_P_WNAME_ACR", R.drawable.w_lineart_acw_r);
+            put("WARSAW_ID_P_WNAME_AK5C", R.drawable.w_lineart_ak5c);
+            put("WARSAW_ID_P_WNAME_QBS09", R.drawable.w_lineart_qbs09);
+            put("WARSAW_ID_P_WNAME_AK12", R.drawable.w_lineart_ak12);
+            put("WARSAW_ID_P_WNAME_RFBTARGET", R.drawable.w_lineart_rfb);
+            put("WARSAW_ID_P_WNAME_M412REX", R.drawable.w_lineart_m412_rex);
+            put("WARSAW_ID_P_WNAME_QBZ951", R.drawable.w_lineart_qb95_1);
+            put("WARSAW_ID_P_XP1_WNAME_L96A1", R.drawable.w_lineart_l96a1);
+            put("WARSAW_ID_P_WNAME_MX4", R.drawable.w_lineart_mx4);
+            put("WARSAW_ID_P_WNAME_P226", R.drawable.w_lineart_p226);
+
             put("WARSAW_ID_P_WNAME_JNG90", R.drawable.w_jng90_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE2", R.drawable.w_knife_scout_fancy);
-            put("WARSAW_ID_P_INAME_V40", R.drawable.w_v40_mini_fancy);
-            put("WARSAW_ID_P_INAME_SRAW", R.drawable.w_fgm172_sraw_fancy);
-            put("WARSAW_ID_P_INAME_CLAYMORE", R.drawable.w_claymore_fancy);
-            put("WARSAW_ID_P_INAME_M67", R.drawable.w_m67_grenade_fancy);
             put("WARSAW_ID_P_WNAME_SG553", R.drawable.w_sg553lb_fancy);
-            put("WARSAW_ID_P_INAME_M2", R.drawable.w_m2_slam_fancy);
             put("WARSAW_ID_P_WNAME_SRS", R.drawable.w_srs_338recon_fancy);
             put("WARSAW_ID_P_WNAME_M82A3CQB", R.drawable.w_m82a3_cqb_fancy);
-            put("WARSAW_ID_P_INAME_FGM148", R.drawable.w_fgm148_javelin_fancy);
             put("WARSAW_ID_P_WNAME_USAS12", R.drawable.w_usas12_bp_fancy);
             put("WARSAW_ID_P_WNAME_M82A3", R.drawable.w_m82a3_far_fancy);
-            put("WARSAW_ID_P_INAME_MACHETE", R.drawable.w_machete_fancy);
             put("WARSAW_ID_P_INAME_SMAW", R.drawable.w_mk153_smaw_fancy);
             put("WARSAW_ID_P_WNAME_GALIL53", R.drawable.w_galil_ace53sv_fancy);
-            put("WARSAW_ID_P_INAME_MORTAR", R.drawable.vehicle_m224_mortar);
             put("WARSAW_ID_P_XP1_WNAME_MTAR21", R.drawable.w_mtar21_fancy);
-            put("WARSAW_ID_P_XP1_VNAME_UCAV", R.drawable.vehicle_ucav);
             put("WARSAW_ID_P_XP1_WNAME_RPK74", R.drawable.w_rpk74_fancy);
             put("WARSAW_ID_P_WNAME_M39", R.drawable.w_m39_fancy);
             put("WARSAW_ID_P_WNAME_PECHENEG", R.drawable.w_pecheneg_fancy);
@@ -58,10 +38,7 @@ public class WeaponImageMap {
             put("WARSAW_ID_P_WNAME_QBU88", R.drawable.w_qbu88_fancy);
             put("WARSAW_ID_P_WNAME_STEYRAUG", R.drawable.w_steyraug_fancy);
             put("WARSAW_ID_P_WNAME_G36C", R.drawable.w_g36c_fancy);
-            put("WARSAW_ID_P_INAME_ACB90", R.drawable.w_knife_combat_acb90_fancy);
             put("WARSAW_ID_P_WNAME_HK45C", R.drawable.w_hk45c_fancy);
-            put("WARSAW_ID_P_INAME_REPAIR", R.drawable.w_repairtool_fancy);
-            put("WARSAW_ID_P_INAME_XM25_SMK", R.drawable.w_xm25_fancy);
             put("WARSAW_ID_P_WNAME_SV98", R.drawable.w_sv98_fancy);
             put("WARSAW_ID_P_WNAME_MG4", R.drawable.w_mg4_fancy);
             put("WARSAW_ID_P_WNAME_SAIGA12", R.drawable.w_saiga12_fancy);
@@ -76,13 +53,10 @@ public class WeaponImageMap {
             put("WARSAW_ID_P_WNAME_SCARHSV", R.drawable.w_scarhsv_fancy);
             put("WARSAW_ID_P_WNAME_M40A5", R.drawable.w_m40a5_fancy);
             put("WARSAW_ID_P_WNAME_SKS", R.drawable.w_sks_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE4", R.drawable.w_knife_carbon_fancy);
             put("WARSAW_ID_P_WNAME_FYJS", R.drawable.w_fyjs_fancy);
             put("WARSAW_ID_P_WNAME_M1911", R.drawable.w_m1911_fancy);
             put("WARSAW_ID_P_WNAME_TYPE95B1", R.drawable.w_type95b1_fancy);
-            put("WARSAW_ID_P_INAME_40MM_SMK", R.drawable.w_m320_fancy);
             put("WARSAW_ID_P_WNAME_M16A4", R.drawable.w_m16a4_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE3", R.drawable.w_knife_trench_fancy);
             put("WARSAW_ID_P_WNAME_M1014", R.drawable.w_m1014_fancy);
             put("WARSAW_ID_P_WNAME_AEK971", R.drawable.w_aek971_fancy);
             put("WARSAW_ID_P_WNAME_TAURUS44", R.drawable.w_taurus44_fancy);
@@ -91,9 +65,7 @@ public class WeaponImageMap {
             put("WARSAW_ID_P_WNAME_CZ805", R.drawable.w_cz805_fancy);
             put("WARSAW_ID_P_WNAME_SCOUTELIT", R.drawable.w_scoutelite_fancy);
             put("WARSAW_ID_P_WNAME_SHORTY", R.drawable.w_shorty12g_fancy);
-            put("WARSAW_ID_P_INAME_40MM_SHG", R.drawable.w_m320_fancy);
             put("WARSAW_ID_P_WNAME_RPK12", R.drawable.w_rpk12_fancy);
-            put("WARSAW_ID_P_INAME_DEFIB", R.drawable.w_defib_fancy);
             put("WARSAW_ID_P_WNAME_SPAS12", R.drawable.w_spas12_fancy);
             put("WARSAW_ID_P_WNAME_M249", R.drawable.w_m249_fancy);
             put("WARSAW_ID_P_WNAME_A91", R.drawable.w_a91_fancy);
@@ -102,11 +74,8 @@ public class WeaponImageMap {
             put("WARSAW_ID_P_WNAME_93R", R.drawable.w_m93r_fancy);
             put("WARSAW_ID_P_INAME_XM25_FLECHETTE", R.drawable.w_xm25_fancy);
             put("WARSAW_ID_P_XP1_WNAME_MP7", R.drawable.w_mp7_fancy);
-            put("WARSAW_ID_P_INAME_40MM_LVG", R.drawable.w_m320_fancy);
             put("WARSAW_ID_P_WNAME_870", R.drawable.w_remington870_fancy);
-            put("WARSAW_ID_P_INAME_M15", R.drawable.w_at_mine_fancy);
             put("WARSAW_ID_P_WNAME_AMR2CQB", R.drawable.w_amr_cqb_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE6", R.drawable.w_knife_bowie_fancy);
             put("WARSAW_ID_P_INAME_M26_FRAG", R.drawable.w_m26mass_fancy);
             put("WARSAW_ID_P_WNAME_UTAS", R.drawable.w_utas_fancy);
             put("WARSAW_ID_P_INAME_M34", R.drawable.w_m34_incendiary_fancy);
@@ -122,69 +91,44 @@ public class WeaponImageMap {
             put("WARSAW_ID_P_WNAME_SVD12", R.drawable.w_svd12_fancy);
             put("WARSAW_ID_P_WNAME_CZ75", R.drawable.w_cz75_fancy);
             put("WARSAW_ID_P_INAME_M18", R.drawable.w_m18_smoke_fancy);
-            put("WARSAW_ID_P_WNAME_TYPE88", R.drawable.w_type88_fancy);
-            put("WARSAW_ID_P_INAME_SHANK", R.drawable.w_shank_fancy);
-            put("WARSAW_ID_P_INAME_IGLA", R.drawable.w_sa18_igla_fancy);
             put("WARSAW_ID_P_INAME_M26_SLUG", R.drawable.w_m26mass_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE5", R.drawable.w_knife_survival_fancy);
             put("WARSAW_ID_P_WNAME_M240", R.drawable.w_m240_fancy);
             put("WARSAW_ID_P_WNAME_QSZ92", R.drawable.w_qsz_92_fancy);
             put("WARSAW_ID_P_WNAME_PDR", R.drawable.w_magpulpdr_fancy);
-            put("WARSAW_ID_P_WNAME_AMR2MID", R.drawable.w_amr_med_fancy);
             put("WARSAW_ID_P_XP1_WNAME_L85A2", R.drawable.w_l85a2_fancy);
             put("WARSAW_ID_P_WNAME_UMP9", R.drawable.w_ump9_fancy);
             put("WARSAW_ID_P_WNAME_M200", R.drawable.w_m200_srr61_fancy);
-            put("WARSAW_ID_P_INAME_M26_FLECHETTE", R.drawable.w_m26mass_fancy);
-            put("WARSAW_ID_P_INAME_FLASHBANG", R.drawable.w_m84_flashbang_fancy);
             put("WARSAW_ID_P_WNAME_M9", R.drawable.w_m9_fancy);
             put("WARSAW_ID_P_WNAME_GALIL23", R.drawable.w_galil_ace23_fancy);
-            put("WARSAW_ID_P_INAME_40MM_FLASH", R.drawable.w_m320_fancy);
             put("WARSAW_ID_P_INAME_M136", R.drawable.w_m136_fancy);
             put("WARSAW_ID_P_INAME_STARSTREAK", R.drawable.w_hvm_starstreak_fancy);
             put("WARSAW_ID_P_WNAME_M4A1", R.drawable.w_m4a1_fancy);
             put("WARSAW_ID_P_WNAME_FN57", R.drawable.w_fn57_fancy);
             put("WARSAW_ID_P_WNAME_JS2", R.drawable.w_js2_fancy);
-            put("WARSAW_ID_P_INAME_FLARE", R.drawable.w_handflare_fancy);
-            put("WARSAW_ID_P_INAME_EOD", R.drawable.vehicle_eodbot);
-            put("WARSAW_ID_P_XP4_INAME_KNIFEWEAVER", R.drawable.w_knife_weaver_fancy);
 
             /*SECOND ASSAULT*/
             put("WARSAW_ID_P_XP0_WNAME_DAO12", R.drawable.w_dao12_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE1", R.drawable.w_knife_improvised_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE8", R.drawable.w_knife_sealknife_fancy);
             put("WARSAW_ID_P_XP0_WNAME_GOL", R.drawable.w_gol_fancy);
             put("WARSAW_ID_P_XP0_WNAME_F2000", R.drawable.w_f2000_fancy);
             put("WARSAW_ID_P_XP0_WNAME_M60E4", R.drawable.w_m60e4_fancy);
             put("WARSAW_ID_P_XP0_WNAME_ASVAL", R.drawable.w_asval_fancy);
-            put("WARSAW_ID_P_INAME_BPKNIFE7", R.drawable.w_knife_bootknife_fancy);
 
             /*NAVAL STRIKE*/
             put("WARSAW_ID_P_XP2_WNAME_SR2", R.drawable.w_sr2_fancy);
             put("WARSAW_ID_P_XP2_WNAME_AR160", R.drawable.w_ar160_fancy);
             put("WARSAW_ID_P_XP2_WNAME_AWS", R.drawable.w_aws_fancy);
             put("WARSAW_ID_P_XP2_WNAME_SR338", R.drawable.w_sr338_fancy);
-            put("WARSAW_ID_P_XP2_INAME_AAMINE", R.drawable.w_aamine_fancy);
-            put("WARSAW_ID_P_INAME_DIVERKNIFE", R.drawable.w_knife_diver_fancy);
-            put("WARSAW_ID_P_INAME_KNIFE14100BT", R.drawable.w_knife_tactical2_fancy);
-            put("WARSAW_ID_P_XP2_INAME_40MM_3GL", R.drawable.w_m320_fancy);
             put("WARSAW_ID_P_XP2_WNAME_SW40", R.drawable.w_sw40_fancy);
-            put("WARSAW_ID_P_INAME_KNIFE2142", R.drawable.w_knife_2142_fancy);
-            put("WARSAW_ID_P_INAME_KNIFEPRECISION", R.drawable.w_knife_precision_fancy);
 
             /*DRAGON'S TEETH*/
             put("WARSAW_ID_P_XP3_WNAME_UN6", R.drawable.w_unica6_fancy);
             put("WARSAW_ID_P_XP3_WNAME_MPX", R.drawable.w_mpx_fancy);
             put("WARSAW_ID_P_XP3_WNAME_DEAGLE", R.drawable.w_deserteagle_fancy);
-            put("WARSAW_ID_P_INAME_NECKKNIFE", R.drawable.w_knife_neck_fancy);
             put("WARSAW_ID_P_XP3_WNAME_BULLDOG", R.drawable.w_bulldog_fancy);
             put("WARSAW_ID_P_XP3_WNAME_CS5", R.drawable.w_cs5_fancy);
-            put("WARSAW_ID_P_XP3_WNAME_SHIELD", R.drawable.w_shield_fancy);
 
             /*FINAL STAND*/
             put("WARSAW_ID_P_XP4_WNAME_RAILGUN", R.drawable.w_railgun_fancy);
-            put("WARSAW_ID_P_XP4_INAME_KNIFETANTO", R.drawable.w_knife_tanto_fancy);
-            put("WARSAW_ID_P_XP4_INAME_KNIFEBIPOD", R.drawable.v_knife_c100_fancy);
-            put("WARSAW_ID_P_XP4_WNAME_SPYDER", R.drawable.w_sh_fancy);
         }
     };
 
--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/battlepacks/WeaponBattlepack.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/battlepacks/WeaponBattlepack.java
@@ -10,6 +10,7 @@ import com.ninetwozero.bf4intel.R;
 import com.ninetwozero.bf4intel.json.battlepacks.WeaponPack;
 import com.ninetwozero.bf4intel.ui.BaseListItem;
 import com.ninetwozero.bf4intel.ui.SimpleListAdapter;
+import com.squareup.picasso.Picasso;
 
 public class WeaponBattlepack implements BaseListItem {
 
@@ -28,16 +29,27 @@ public class WeaponBattlepack implements BaseListItem {
 
     @Override
     public View getView(LayoutInflater inflater, View view) {
+        WeaponPackHolder holder;
         if(view == null) {
             view = inflater.inflate(R.layout.list_item_weapon_battlepack, null);
+            holder = getWeaponPackHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (WeaponPackHolder) view.getTag();
         }
 
+        Picasso picasso = Picasso.with(context);
+        picasso.load(weaponPack.getWeaponInfo().getUniqueId()).into(holder.weaponImage);
         return view;
     }
 
     private WeaponPackHolder getWeaponPackHolder(View view) {
         WeaponPackHolder holder = new WeaponPackHolder();
-        holder.
+        holder.weaponImage = (ImageView) view.findViewById(R.id.weapon_image);
+        holder.battlepackImage = (ImageView) view.findViewById(R.id.battlepack_image);
+        holder.weaponName = (TextView) view.findViewById(R.id.weapon_name);
+        holder.valueNeeded = (TextView) view.findViewById(R.id.value_needed);
+        return holder;
     }
 
     private static class WeaponPackHolder {
