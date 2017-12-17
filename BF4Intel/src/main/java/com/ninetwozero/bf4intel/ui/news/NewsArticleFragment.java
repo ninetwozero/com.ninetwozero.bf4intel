f5711a505e3db66fd11d1668cffbca7f2667a1ce
Peter Miklosko <peter.budo@gmail.com>
2017-12-17 16:57:29 +0000

    Project update - project structure, dependencies. Replaced deprecated ActionBarActivity with AppCompatActivity

--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/news/NewsArticleFragment.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/news/NewsArticleFragment.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -133,7 +133,7 @@ public class NewsArticleFragment extends BaseLoadingFragment {
     }
 
     private void setupActionBar() {
-        final ActionBar actionBar = ((ActionBarActivity)getActivity()).getSupportActionBar();
+        final ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
         if (actionBar == null) {
             return;
         }
--- BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/settings/SettingsActivity.java
+++ BF4Intel/src/main/java/com/ninetwozero/bf4intel/ui/settings/SettingsActivity.java
@@ -2,11 +2,11 @@ package com.ninetwozero.bf4intel.ui.settings;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 /* Unfortunately, this class is needed due to the lack of a support.v4 PreferenceFragment */
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
